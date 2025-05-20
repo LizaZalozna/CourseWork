191dd6ad58f6fa7fa39c2c5be3881651b9c4fb3f
lizazalozna <liza.zalozna@gmail.com>
2025-05-20 18:45:19 +0300

    bookGenreClass

---
+++ CourseWork/CourseWork/BookGenre.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace CourseWork
+{
+    public static class BookGenre
+    {
+        public enum LiteraryGenre
+        {
+            Crime,
+            Fantasy,
+            Horror,
+            Romance,
+            Historical,
+            Adventure,
+            ScienceFiction,
+            Mystery,
+            Thriller,
+            Drama,
+            Comedy,
+            Biography,
+            Autobiography,
+            Dystopian,
+            Utopian,
+            Classic,
+            Poetry,
+            Satire,
+            NonFiction,
+            Children,
+            FairyTale,
+            Mythology,
+            GraphicNovel
+        }
+    }
+}
+
