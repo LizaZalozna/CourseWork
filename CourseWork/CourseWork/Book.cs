627f13d2021c1272073c722d0413e9d86f80090d
lizazalozna <liza.zalozna@gmail.com>
2025-05-21 12:00:55 +0300

    reservationAndLendingLogicForBook

--- CourseWork/CourseWork/Book.cs
+++ CourseWork/CourseWork/Book.cs
@@ -6,13 +6,60 @@ namespace CourseWork
         string fullNameOfAutor;
         string nameOfBook;
         BookGenre.LiteraryGenre genre;
+        bool isAvailable;
+        bool isReserved;
+        SimpleUser reservedBy;
+
+        public bool isAvailable_
+        {
+            get { return isAvailable; }
+        }
+
+        public bool isReserved_
+        {
+            get { return isReserved; }
+        }
+
+        public SimpleUser reservedBy_
+        {
+            get { return reservedBy; }
+        }
 
         public Book(string fullName, string nameOfBook, BookGenre.LiteraryGenre genre)
         {
             this.fullNameOfAutor = fullName;
             this.nameOfBook = nameOfBook;
             this.genre = genre;
+            this.isAvailable = true;
+            this.isReserved = false;
+        }
+
+        public bool Reserve(SimpleUser user)
+        {
+            if (isAvailable && !isReserved)
+            {
+                isReserved = true;
+                reservedBy = user;
+                return true;
+            }
+            return false;
+        }
+
+        public void CancelReservation()
+        {
+            isReserved = false;
+            reservedBy = null;
+        }
+
+        public void Lend()
+        {
+            isAvailable = false;
+            CancelReservation();
+        }
+
+        public void Return()
+        {
+            isAvailable = true;
         }
     }
 }
-
