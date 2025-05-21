2fe91d526c26ea46b6fdeee4c7be1cb51fbd8239
lizazalozna <liza.zalozna@gmail.com>
2025-05-21 12:49:11 +0300

    recordsClasses

---
+++ CourseWork/CourseWork/Record.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace CourseWork
+{
+    public class LendingRecord
+    {
+        Book book;
+        DateTime lendDate;
+        DateTime returnDate;
+
+        public Book book_
+        {
+            get { return book; }
+        }
+
+        public DateTime lendDate_
+        {
+            get { return lendDate; }
+        }
+
+        public DateTime returnDate_
+        {
+            get { return returnDate; }
+            set
+            {
+                if (value >= lendDate) returnDate = value;
+                else throw new Exception("Дата повернення не може бути раніше дати видачі книги");
+            }
+        }
+
+        public LendingRecord(Book book, DateTime lendDate)
+        {
+            this.book = book;
+            this.lendDate = lendDate;
+        }
+    }
+
+    public class ReservationRecord
+    {
+        Book book;
+        DateTime reservationDate;
+
+        public Book book_
+        {
+            get { return book; }
+        }
+
+        public DateTime reservationDate_
+        {
+            get { return reservationDate; }
+        }
+
+        public ReservationRecord(Book book, DateTime reservationDate)
+        {
+            this.book = book;
+            this.reservationDate = reservationDate;
+        }
+    }
+}
+
